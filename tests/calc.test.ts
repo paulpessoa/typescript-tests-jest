e31f5e4007b415c99caf46dcd399ee66413aeb97
Paul Pessoa <paulmspessoa@gmail.com>
2024-01-04 14:54:11 -0300

    multiply test

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -1,4 +1,4 @@
-import {add} from "../src/calc"
+import {add, multiply} from "../src/calc"
 
 describe("test calc", () => {
   it("return 15 for add(10,5)", () => {
@@ -6,5 +6,14 @@ describe("test calc", () => {
   })
   it("return 5 for add(2,3)", () => {
     expect(add(2,3)).toBe(5)
+  })
+})
+
+describe('test multiply', () => { 
+  it('return 20 for mult(4,5)', () => {
+    expect(multiply(4,5)).toBe(20)
+  })
+  it('return 6 for multiply(2,3)', () => {
+    expect(multiply(2,3)).toBe(6)
   })
 })
