4074718ed09e6c1448b1c09d5229d20ed50588c4
Paul Pessoa <paulmspessoa@gmail.com>
2024-01-04 14:35:06 -0300

    another test

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -4,4 +4,7 @@ describe("test calc", () => {
   it("return 15 for add(10,5)", () => {
     expect(add(10,5)).toBe(15)
   })
+  it("return 5 for add(2,3)", () => {
+    expect(add(2,3)).toBe(5)
+  })
 })
